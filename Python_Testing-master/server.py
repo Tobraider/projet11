0fa43c0e88ec316f2e606f2d15c9c04d6e111ca7
Tobraider <96182904+Tobraider@users.noreply.github.com>
2023-06-23 16:11:21 +0200

    showSummary_Patch

--- Python_Testing-master/server.py
+++ Python_Testing-master/server.py
@@ -26,8 +26,12 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    club = [club for club in clubs if club['email'] == request.form['email']]
+    if club != []:
+        club = club[0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    else:
+        return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
