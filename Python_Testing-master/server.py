6c9dc83d99f0d66192123af516ef317d9d68c694
Tobraider <96182904+Tobraider@users.noreply.github.com>
2023-06-26 11:07:46 +0200

    un peu plus simple comme ca

--- Python_Testing-master/server.py
+++ Python_Testing-master/server.py
@@ -27,7 +27,7 @@ def index():
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']]
-    if club != []:
+    if club:
         club = club[0]
         return render_template('welcome.html',club=club,competitions=competitions)
     else:
